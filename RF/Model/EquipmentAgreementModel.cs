0c1ee1aceb3377d78b1ee9c0f1eceeba9f401e8c
zxlgrandia <grandia-4727@163.com>
2019-04-23 15:39:19 +0800

    Create EquipmentAgreementModel.cs

---
+++ RF/Model/EquipmentAgreementModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1.Model
+{
+    public class EquipmentAgreementModel
+    {
+        public string Id { get; set; }
+        public string AgreementType { get; set; }
+    }
+}
