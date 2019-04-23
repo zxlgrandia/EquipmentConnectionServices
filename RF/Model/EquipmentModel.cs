cac1a8b06acb20ae2143741396ee3365deb6ba99
18700085881 <1224812884@qq.com>
2019-04-23 15:58:36 +0800

    实体+数据库帮助类

---
+++ RF/Model/EquipmentModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1.Model
+{
+    class EquipmentModel
+    {
+        public string Id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 生产厂家
+        /// </summary>
+        public string Manufacturer { get; set; }
+        /// <summary>
+        /// 型号
+        /// </summary>
+        public string Model { get; set; }
+        /// <summary>
+        ///所属客户端 
+        /// </summary>
+        public string ClientIp { get; set; }
+        /// <summary>
+        /// 协议编号
+        /// </summary>
+        public string AgreementId { get; set; }
+
+    }
+}
---
+++ RF/Model/EquipmentTypeModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1.Model
+{
+    class EquipmentTypeModel
+    {
+        public string Id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string TypeName { get; set; }
+        /// <summary>
+        /// 父类型编号
+        /// </summary>
+        public string ParentId { get; set; }
+        /// <summary>
+        /// 设备编号
+        /// </summary>
+        public string EquipmentId { get; set; }
+        /// <summary>
+        /// 规格型号
+        /// </summary>
+        public string ModelNumber { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+    
+    }
+}
