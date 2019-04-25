6379986505877c58b2d8a372952538325427ae3b
18700085881 <1224812884@qq.com>
2019-04-25 11:05:11 +0800

    dal完善

--- RF/DAL/EquipmentDAL.cs
+++ RF/DAL/EquipmentDAL.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using Utils;
 
 namespace DAL
 {
     public class EquipmentDAL
     {
+        public static List<EquipmentModel> GetEquipmentAgreementList(string clientIp)
+        {
+            var connectionString = ConfigurationManager.AppSettings["DbConnection"];
+            var sqlString = "SELECT * FROM SYS_Equipment WHERE  Client_Ip = @ClientIp";
+            SqlParameter[] sqlParams = new SqlParameter[] {
+                new SqlParameter("@ClientIp", clientIp)
+            };
+            List<EquipmentModel> list = new List<EquipmentModel>();
+            SqlDataReader reader = SqlHelper.ExecuteReader(connectionString, CommandType.Text, sqlString, sqlParams);
+            while (reader.Read())
+            {
+                EquipmentModel e = new EquipmentModel
+                {
+                    
+                    Name = reader.GetString(0),
+                    Manufacturer = reader.GetString(1),
+                    Model = reader.GetString(2),
+                    ClientIp = reader.GetString(3),
+                    AgreementId = reader.GetString(4),
+                    Id = reader.GetString(5),
+
+                };
+                list.Add(e);
+            }
+            reader.Close();
+            return list;
+        }
     }
 }
--- RF/DAL/EquipmentTypeDAL.cs
+++ RF/DAL/EquipmentTypeDAL.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using Utils;
 
 namespace DAL
 {
     public class EquipmentTypeDAL
     {
+        public static List<EquipmentTypeModel> GetEquipmentAgreementList(string clientIp)
+        {
+            var connectionString = ConfigurationManager.AppSettings["DbConnection"];
+            var sqlString = "SELECT * FROM SYS_Equipment WHERE  Client_Ip = @ClientIp";
+            SqlParameter[] sqlParams = new SqlParameter[] {
+                new SqlParameter("@ClientIp", clientIp)
+            };
+            List<EquipmentTypeModel> list = new List<EquipmentTypeModel>();
+            SqlDataReader reader = SqlHelper.ExecuteReader(connectionString, CommandType.Text, sqlString, sqlParams);
+            while (reader.Read())
+            {
+                EquipmentTypeModel e = new EquipmentTypeModel
+                {
+
+                    TypeName = reader.GetString(0),
+                    ParentId = reader.GetString(1),
+                    EquipmentId = reader.GetString(2),
+                    ModelNumber = reader.GetString(3),
+                    Remark = reader.GetString(4),
+                    Id = reader.GetString(5),
+
+                };
+                list.Add(e);
+            }
+            reader.Close();
+            return list;
+        }
 
     }
 }
